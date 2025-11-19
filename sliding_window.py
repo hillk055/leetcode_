92142228611fafb84bff4733dd5985d46ac35592
hillk055 <101193869+hillk055@users.noreply.github.com>
2025-11-19 02:36:03 +0000

    Update sliding_window.py

--- sliding_window.py
+++ sliding_window.py
@@ -38,5 +38,59 @@ for right in range(len(nums)):
 
 print(shortest_array)
 
+"""Find the longest substring containing at most k unique characters"""
+
+s = "eceba"
+k = 2
+left = 0
+longest_sub_string = 0
+chars = {}
+
+for right in range(len(s)):    # Start adding one to the right window
+
+    if s[right] in chars:
+        chars[s[right]] += 1
+    else:
+        chars[s[right]] = 1    # Store the number of counts of characters, add a key if a new character appears
+
+    if len(chars) > k:    # If we have more than k characters
+        while len(chars) > k:    # While more than k characters, delete the first character on the left and increment left by 1, keep going until only k unique characters remain
+            chars[s[left]] -= 1
+            if chars[s[left]] == 0:
+                del chars[s[left]]    # Because we are using length if the count of a character reaches 0 we need to remove it otherwise the condition will always be satisfied.
+            left += 1
+
+    longest_sub_string = max(longest_sub_string, right - left + 1)    # Add after the condition so that we update the longest before we have more than k unique characters
+
+print(longest_sub_string)
+
+"""Find the longest substring containing k unique characters"""
+
+s = "araaci"
+k = 2
+left = 0
+longest_string = 0
+chars = {}
+
+for right in range(len(s)):
+    if s[right] in chars:
+        chars[s[right]] += 1
+    else:
+        chars[s[right]] = 1
+
+    if len(chars) > k:
+        chars[s[left]] -= 1
+        if chars[s[left]] == 0:
+            del chars[s[left]]
+        left += 1
+
+    if len(chars) == 2:
+        longest_string = max(longest_string, right - left + 1)
+
+
+print(longest_string)
+
+
+
 
 
